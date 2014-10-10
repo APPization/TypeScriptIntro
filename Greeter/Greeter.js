bb7cdae462801f9166d0c43355db23a892091ff8
DrOzturk <ozgur.ozturk@cchsfs.com>
2014-10-10 10:51:22 -0400

    Initial Checkin for simple JS example with unintended result

---
+++ Greeter/Greeter.js
@@ -0,0 +1,15 @@
+﻿function Greeter(greeting) {
+    this.greeting = greeting;
+}
+
+Greeter.prototype.greet = function () {
+    return "Hello" + this.greeting;
+}
+
+//Example of simple errors not catched when working with JS
+var greeter = new Greeter({ audience: "Code Campers" });
+
+var greetDiv = document.createElement("div");
+greetDiv.innerHTML = greeter.greet();
+
+document.body.appendChild(greetDiv);
