95ed4976c87a74e074aae2310bb7cdc60e700c63
DrOzturk <ozgur.ozturk@cchsfs.com>
2014-10-16 10:09:44 -0400

    add module example

--- GreeterTSwInterface/Greeter.ts
+++ GreeterTSwInterface/Greeter.ts
@@ -1,23 +1,30 @@
-﻿class Student {
-    constructor(public firstname: string, public lastname: string) {
-    }
+﻿module School {
+// [[1]] try as is without implements;
+//       duck typing
+// [[2]] addd
+// implements Person
+    export class Student{
+        constructor(public firstname: string, public lastname: string) {
+        }
+
 //comment out fullname() and see the mismatch error due to duck typing
 //You don't have to explicitly implement an interface
-    fullname(){
-        return this.firstname + " " + this.lastname;    
+        fullname() {
+            return this.firstname + " " + this.lastname;
+        }
+    }
+
+    export interface Person {
+        firstname: string;
+        lastname: string;
+        fullname();
     }
 }
 
-interface Person {
-    firstname: string;
-    lastname: string;
-    fullname();
-}
-
-function greet(person: Person) {
+function greet(person: School.Person) {
     return "Hello, " + person.fullname();
 }
 
-var user = new Student("Jane", "Smith");
+var user = new School.Student("Jane", "Smith");
 
 document.body.innerHTML = greet(user);
